fcbfe9e45b4678795514334c08db3250c1a097d2
AlanJSimpson <allanstark2908@gmail.com>
2022-04-08 22:50:33 -0300

    Contact page finish with email and whatsapp logic implemented

--- src/page/contactUs/Contact.jsx
+++ src/page/contactUs/Contact.jsx
@@ -1,3 +1,5 @@
+import { init } from '@emailjs/browser';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import {
   Box,
   Button,
@@ -15,13 +17,32 @@ const FACTORY_IMAGE =
   'https://images.unsplash.com/photo-1566688045489-95fddaab0c28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80';
 
 export default function Contact() {
-  const [selectValue, setSelectValue] = useState('');
+  init(process.env.REACT_APP_USER_ID);
+
+  const [sector, setSector] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
+  const [showWhatsAppScreen, setShowWhatsAppScreen] = useState(false);
+
   const handleSelectChange = (newSelectValue) => {
-    setSelectValue(newSelectValue);
+    setSector(newSelectValue);
+  };
+
+  const sendEmail = () => {
+    /*  emailjs.send(
+      process.env.REACT_APP_SERVICE_ID,
+      process.env.REACT_APP_TEMPLATE_ID,
+      {
+        sector,
+        name,
+        email,
+        subject,
+        message,
+      }
+    ); */
+    setShowWhatsAppScreen(true);
   };
 
   return (
@@ -82,65 +103,120 @@ export default function Contact() {
                   paddingY: '20px',
                 }}
               >
-                <Box>
-                  <SimpleSelect
-                    label='Selecione setor para contato'
-                    value={selectValue}
-                    onSelectChange={handleSelectChange}
-                    options={[
-                      { title: 'Engenharia', value: 'Engenharia' },
-                      { title: 'Comercial', value: 'Comercial' },
-                      { title: 'Treinamentos', value: 'Treinamentos' },
-                    ]}
-                  />
-                </Box>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    gap: '8px',
-                  }}
-                >
-                  <TextField
-                    value={name}
-                    onInput={(e) => setName(e.target.value)}
-                    label='Nome'
-                    size='small'
-                  />
-                  <TextField
-                    value={email}
-                    onInput={(e) => setEmail(e.target.value)}
-                    label='E-mail'
-                    size='small'
-                  />
-                  <TextField
-                    value={subject}
-                    onInput={(e) => setSubject(e.target.value)}
-                    label='Assunto'
-                    size='small'
-                  />
-                </Box>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    width: '100%',
-                  }}
-                >
-                  <TextField
-                    label='Mensagem'
-                    width='100%'
-                    multiline
-                    rows={4}
-                    value={message}
-                    onInput={(e) => setMessage(e.target.value)}
-                  />
-                </Box>
-                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                  <Button variant='contained'>Enviar</Button>
-                </Box>
+                {showWhatsAppScreen ? (
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      gap: '3rem',
+                    }}
+                  >
+                    <Typography
+                      color='secondary'
+                      component='h2'
+                      variant='h5'
+                      fontWeight='bold'
+                      textAlign='center'
+                    >
+                      Você também pode nos contatar por whatsapp
+                    </Typography>
+                    <Box
+                      sx={{ display: 'flex', justifyContent: 'space-around' }}
+                    >
+                      <Button
+                        onClick={() => setShowWhatsAppScreen(false)}
+                        variant='contained'
+                        color='secondary'
+                      >
+                        Voltar
+                      </Button>
+                      <Button variant='contained' startIcon={<WhatsAppIcon />}>
+                        <a
+                          target='_blank'
+                          href={
+                            sector === 'Engenharia'
+                              ? `https://wa.me/55${process.env.REACT_APP_TEL_ENGENHARIA}`
+                              : sector === 'Comercial' ||
+                                sector === 'Treinamentos'
+                              ? `https://wa.me/55${process.env.REACT_APP_TEL_COMERCIAL}`
+                              : `https://wa.me/55${process.env.REACT_APP_TEL_SOLICITE_VISITA}`
+                          }
+                          rel='noreferrer'
+                        >
+                          WhatsApp
+                        </a>
+                      </Button>
+                    </Box>
+                  </Box>
+                ) : (
+                  <>
+                    <Box>
+                      <SimpleSelect
+                        label='Selecione setor para contato'
+                        value={sector}
+                        onSelectChange={handleSelectChange}
+                        options={[
+                          { title: 'Engenharia', value: 'Engenharia' },
+                          { title: 'Comercial', value: 'Comercial' },
+                          { title: 'Treinamentos', value: 'Treinamentos' },
+                          {
+                            title: 'Solicite uma visita',
+                            value: 'Solicite uma visita',
+                          },
+                        ]}
+                      />
+                    </Box>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        gap: '8px',
+                      }}
+                    >
+                      <TextField
+                        value={name}
+                        onInput={(e) => setName(e.target.value)}
+                        label='Nome'
+                        size='small'
+                      />
+                      <TextField
+                        value={email}
+                        onInput={(e) => setEmail(e.target.value)}
+                        label='E-mail'
+                        size='small'
+                      />
+                      <TextField
+                        value={subject}
+                        onInput={(e) => setSubject(e.target.value)}
+                        label='Assunto'
+                        size='small'
+                      />
+                    </Box>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        width: '100%',
+                      }}
+                    >
+                      <TextField
+                        label='Mensagem'
+                        width='100%'
+                        multiline
+                        rows={4}
+                        value={message}
+                        onInput={(e) => setMessage(e.target.value)}
+                      />
+                    </Box>
+                    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                      <Button onClick={sendEmail} variant='contained'>
+                        Enviar
+                      </Button>
+                    </Box>
+                  </>
+                )}
               </Box>
             </Grid>
           </Grid>
